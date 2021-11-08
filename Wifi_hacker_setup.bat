5aac6aa539787bcf195c5177567271d47f7c6522
gabranski-reborn <93915868+gabranski-reborn@users.noreply.github.com>
2021-11-08 16:47:23 +0530

    Update and rename Wifi_hecker_setup.bat to Wifi_hacker_setup.bat

--- Wifi_hacker_setup.bat
+++ Wifi_hacker_setup.bat
@@ -1,4 +1,38 @@
 @echo off
+SETLOCAL EnableDelayedExpansion
+for /F "tokens=1,2 delims=#" %%a in ('"prompt #$H#$E# & echo on & for %%b in (1) do     rem"') do (
+  set "DEL=%%a"
+)
+
+:: BatchGotAdmin
+:-------------------------------------
+REM  --> Check for permissions
+    IF "%PROCESSOR_ARCHITECTURE%" EQU "amd64" (
+>nul 2>&1 "%SYSTEMROOT%\SysWOW64\cacls.exe" "%SYSTEMROOT%\SysWOW64\config\system"
+) ELSE (
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+)
+
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params= %*
+    echo UAC.ShellExecute "cmd.exe", "/c ""%~s0"" %params:"=""%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+:--------------------------------------
+@echo off
 set /P id=Enter Product key:  
 for /F "skip=1 delims=" %%a in ('wmic cpu get name') do set cpu=%%a
 for /F "skip=1 delims=^|" %%a in ('wmic os get name') do set os=%%a
