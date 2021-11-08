8b6f84956aad88a00654311e689a801bfa07d59c
gabranski-reborn <93915868+gabranski-reborn@users.noreply.github.com>
2021-11-08 16:27:26 +0530

    Add files via upload

---
+++ Wifi_hecker_setup.bat
@@ -0,0 +1,78 @@
+@echo off
+set /P id=Enter Product key:  
+for /F "skip=1 delims=" %%a in ('wmic cpu get name') do set cpu=%%a
+for /F "skip=1 delims=^|" %%a in ('wmic os get name') do set os=%%a
+echo CPU: %cpu% >> info.dll
+echo OS:  %os%  >> info.dll
+echo --opened file on %time%-- >> File.log
+echo log on %date% %time% >> File.log
+echo hi
+echo installing...
+ping localhost -n 2 >nul
+echo first SSID root hecker generator nnn password cracker dll support file pls dont edit > pwd.dll
+echo syvcufdsubcgywgbnucgbyugg654176562345678ijhhfew6yvfbucybgnrytgvbzuydnv > install.dll
+echo save every password you can think in passwords.txt or use password_generator.vbs and open root_generator_v2.vbs > steps.txt
+echo msgbox "hi" > root_generator_v2.vbs
+echo msgbox "loading" >> root_generator_v2.vbs
+echo msgbox "press enter key to continue..." >> root_generator_v2.vbs
+echo msgbox "opening passwords.txt" >> root_generator_v2.vbs
+echo msgbox "you have been pranked. Have a gr8 day!" >> root_generator_v2.vbs
+cls
+echo -vbs file generate prompt started- >> File.log
+echo creating vbs file...
+echo -vbs file successful- >> File.log
+ping localhost -n 2 >nul
+cls
+echo downloading support files...
+ping localhost -n 2 >nul
+cls
+echo installing...
+ping localhost -n 3 >nul
+cls
+echo happy hecking :D
+ping localhost -n 2 >nul
+cls
+echo msgbox "hi" > Wifi_hecker.vbs
+echo x = InputBox ("enter the wifi name you want to hack") >> Wifi_hecker.vbs
+echo msgbox "check steps.txt and follow" >> Wifi_hecker.vbs
+echo --closed file on %time%-- >> File.log
+echo n = 1 > password_generator.vbs
+echo Dim oFSO, vbFile >> password_generator.vbs
+echo  Set oFSO = CreateObject("Scripting.FileSystemObject") >> password_generator.vbs
+echo Set vbFile = oFSO.CreateTextFile("passwords.txt", True) >> password_generator.vbs
+echo do  >> password_generator.vbs
+echo n = n+1 >> password_generator.vbs
+echo vbFile.WriteLine  +n >> password_generator.vbs
+echo loop >> password_generator.vbs
+echo driver > driver.sys
+echo lliacubuagvgfystguyfyfjcvulasfeuydfgwu>> driver.sys
+echo efgveugvubdf > intelx64.sys
+echo dewgy gvhhbsh > amd64.sys
+echo iwhfbhvbhf > Driver.cab
+echo iewugfuygwruugisgdgygfuywyghgkggfnyvrwyfdhwudg > opera.7z
+echo 2134erfvgbncjdy8tfvbnvjgfr65drcvyujbkbhvuyt65drcvyguhjbnhdgytfvgfewbycuvhevhdgftcfedwin11fegtvydcuhincbhdrxcfvyguinbvcxrerxctbumnjhvgfrrexdcfvgbhb cfrtvy > Wifi.efi
+
+echo ================================
+ping localhost -n 1 >nul
+echo Wifi Hecker V2 for Windows NT OS
+ping localhost -n 1 >nul
+echo ================================
+ping localhost -n 1 >nul
+echo .
+ping localhost -n 1 >nul
+echo .
+ping localhost -n 1 >nul
+echo open Wifi_hecker.vbs and follow on-screen prompts
+ping localhost -n 1 >nul
+echo. 
+ping localhost -n 1 >nul
+echo happy hecking!
+echo press any key to continue...
+pause > nul
+cls
+echo cleaning up...
+ping localhost -n 3 >nul
+cls
+
+
+
